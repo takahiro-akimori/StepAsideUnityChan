7c39a6cb13f927b1a43905a66852d7727ccc4a00
あきもりたかひろ <tak0316.a@gmail.com>
2019-10-04 23:23:03 +0900

    StepAsideUnityChan   初コミット
    課題　UnityChanが通り過ぎた障害物を消すDeleteController追加

---
+++ Assets/DeleteController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteController : MonoBehaviour {
+
+	//unitycahn
+	public GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+
+		//unitychanの情報を取得
+		this.unitychan = GameObject.Find("unitychan");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (this.gameObject.transform.position.z < this.unitychan.transform.position.z){
+			Destroy (this.gameObject,0.2f);
+		}
+	}
+}
